8f67011d8ca10cec9b830f2ca2a59e46025e001c
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-14 13:50:05 +0530

    added watchpage

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+  const dispatch = useDispatch();
 
-export default WatchPage
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, [dispatch]);
+
+  return (
+    <div className="">
+      <iframe
+        width="1200"
+        height="600"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
